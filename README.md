309b7f87b07dde55eb55c6df9d39e4c732d191a8
qujianwei <qujianwei@diynova.com>
2021-11-10 21:15:17 +0800

    feat:init

---
+++ README.md
@@ -0,0 +1,22 @@
+### VuePress
+
+https://vuepress.vuejs.org/zh/guide/getting-started.html
+
+```
+# 安装对应的包
+yarn # npm install
+
+# 运行开发环境
+yarn docs:dev # npm run docs:dev
+或
+vuepress dev docs
+
+
+# 构建页面
+yarn docs:build # npm run docs:build
+或
+vuepress build docs
+
+# 目标文件目录
+docs/.vuepress/dist
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Andverse',
+  description: '',
+  themeConfig: {
+    displayAllHeaders: true, // 默认值：false
+    nav: [
+          { text: 'English Version', link: '/en/', ariaLabel: 'English Version' },
+          { text: '中文版', link: '/zh/', ariaLabel: '中文版' },
+        ],
+    sidebar: {
+      '/zh/': [
+        ''
+      ],
+      '/en/': [
+        '',
+      ],
+      '/': ['']
+    },
+  },
+  configureWebpack: (config, isServer) => {
+    if (!isServer) {
+      // 修改客户端的 webpack 配置
+    }
+  }
+}
